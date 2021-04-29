f509cd6af049d191701cfde180a112ba3b6cef6e
isaacsiaw97 <61837489+isaacsiaw97@users.noreply.github.com>
2021-04-29 09:10:34 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+function animateMenu(x){
+	x.classList.toggle("change")
+	var y = document.getElementsByClassName("hiddenNav");
+	y[0].classList.toggle("change")
+	var z = document.getElementsByClassName("navContainer");
+	z[0].classList.toggle("change")
+}
